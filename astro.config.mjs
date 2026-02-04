a51f8fd4c5b014cf3f583e0f94cc3ca62619399c
ddodogames <69091682+ddodogames@users.noreply.github.com>
2026-02-05 01:55:59 +0200

    hi

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import starlightContextualMenu from "starlight-contextual-menu";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://dodo-bot-project.github.io",
-	// base: "/dodo-bot-site",
+	base: "/website",
 	integrations: [
 		starlight({
 			plugins: [
